3ece4f339803f0e01790be88c6501f243aae403b
Andrey <andreydem42@gmail.com>
2025-10-04 16:14:47 +0300

    add json example

---
+++ customers/entity.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://andreydem42_db_user:zrCpoM9uRYBH2jQM@finamhackathon.rs3houu.mongodb.net/")
+db = client["FinamHackathon"]
+collection = db["news"]
+
+data = {
+    "topic": "Прорывные технологии",
+    "draft": {
+      "title": "Генерирация котиков с помощью ИИ",
+      "lead": "Apple создала технологию для мгновенной генерации котиков",
+      "points": [
+        "Генерация проходит за 2 секунды",
+        "Один котик будет стоить 0.01$",
+        "Технология уже доступна всем желающим"
+      ]
+    },
+    "sentiment": 1.0,
+    "hotness": 0.7,
+    "why_hot": "ИИ это перспективное направление, а котиками интересуются все",
+    "why_now": "Новость вызвала манипулятивные движения на рынке",
+    "impact_analysis": "На фоне этой новости и недавних отчетов акции Apple могут вырасти",
+    "statistics": {
+        "compression": 0.5,
+        "quality": 1.0,
+        "processing_started": datetime(2024, 6, 1, 12, 48),
+        "processing_ended": datetime(2024, 6, 1, 15, 37)
+    },
+    "entities": {
+        "companies": ['Apple'],
+        "tickers": ['AAPL', 'WHAT', 'THE', 'FUCK'],
+        "countries": ['USA'],
+        "sectors": ['IT'],
+        "currencies": ['USD'],
+    },
+    "datetime": datetime(2024, 5, 30, 9, 12),
+    "link": "https://catscaptureworld.com",
+}
+
+collection.insert_one(data)
